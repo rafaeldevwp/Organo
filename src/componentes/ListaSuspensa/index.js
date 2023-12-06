dfe8a33751cb98c77a4b362a8da9675735fc623e
rafaeldevwp <fael.loliveira@outlook.com>
2023-12-06 13:45:14 -0300

    Alteracoes 1

--- src/componentes/ListaSuspensa/index.js
+++ src/componentes/ListaSuspensa/index.js
@@ -6,6 +6,7 @@ const ListaSuspensa = (props) => {
         <div className='lista-suspensa'>
             <label>{props.label}</label>
             <select onChange={evento => props.aoAlterado(evento.target.value)} value={props.valor}>
+                <option value=""></option>
             {props.itens.map(item =>  {return <option key={item}>{item}</option>} )}
             </select>
         </div>
